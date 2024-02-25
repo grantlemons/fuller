acdd7b445841fe328c63757fbd150d88617add82
Grant Lemons <grant0lemons@gmail.com>
2024-02-24 17:22:48 -0700

    setup tracing subscriber

--- canvas_cli/src/main.rs
+++ canvas_cli/src/main.rs
@@ -1,12 +1,26 @@
 use anyhow::Context;
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    setup_logging()?;
+
     let auth_token = canvas_auth::connect()
         .await
         .context("Fetching Auth Token Failed!")?;
     let _client =
         canvas_api::create_client(auth_token.secret()).context("Creating Client Failed!")?;
+
+    Ok(())
+}
+
+fn setup_logging() -> anyhow::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .context("Setting default subscriber failed")?;
 
     Ok(())
 }
