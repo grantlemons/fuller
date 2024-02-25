bd1ad2ff5e79d3630e5d5f20de8dabdb6170918f
Grant Lemons <grant0lemons@gmail.com>
2024-02-24 17:55:20 -0700

    Setup logging with tracing and tracing_subscriber (#11)
    - setup tracing subscriber
    - configure tracing for auth crate
    - add logging to api crate
    - pass accesstoken from beginning to end to hide from logging

--- canvas_cli/src/main.rs
+++ canvas_cli/src/main.rs
@@ -1,12 +1,29 @@
 use anyhow::Context;
+use std::{fs::File, sync::Mutex};
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    setup_logging()?;
+
     let auth_token = canvas_auth::connect()
         .await
         .context("Fetching Auth Token Failed!")?;
-    let _client =
-        canvas_api::create_client(auth_token.secret()).context("Creating Client Failed!")?;
+    let _client = canvas_api::create_client(auth_token).context("Creating Client Failed!")?;
+
+    Ok(())
+}
+
+fn setup_logging() -> anyhow::Result<()> {
+    let log_file = File::create("most-recent.log")?;
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .with_writer(Mutex::new(log_file))
+        .pretty()
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .context("Setting default subscriber failed")?;
 
     Ok(())
 }
