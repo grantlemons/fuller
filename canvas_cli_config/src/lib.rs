54a4d52d20627f13674f2d35311a0d459abc2d08
Grant Lemons <grantlemons@aol.com>
2024-02-29 21:25:19 -0700

    create new config crate

---
+++ canvas_cli_config/src/lib.rs
@@ -0,0 +1,71 @@
+use serde::Deserialize;
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ConfigError {
+    #[error("Path is not a file")]
+    InvalidPath,
+    #[error("Unable to open file")]
+    FsError(#[from] std::io::Error),
+    #[error("Unable to parse file")]
+    ParseError(#[from] toml::de::Error),
+}
+
+#[derive(serde::Serialize, Deserialize, Clone)]
+pub struct AccessToken(String);
+
+impl std::fmt::Debug for AccessToken {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "{{Access Token}}")
+    }
+}
+
+impl From<String> for AccessToken {
+    fn from(value: String) -> Self {
+        Self(value)
+    }
+}
+
+impl AccessToken {
+    pub fn new<T: ToString>(value: T) -> Self {
+        Self(value.to_string())
+    }
+
+    pub fn secret(&self) -> &str {
+        &self.0
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub network: NetworkConfig,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct NetworkConfig {
+    pub url: String,
+    pub token: AccessToken,
+}
+
+pub fn get_config(path: Option<PathBuf>) -> Result<Config, ConfigError> {
+    let path = match path {
+        Some(p) => p,
+        None => PathBuf::from("./config.toml"),
+    };
+    if !path.is_file() {
+        return Err(ConfigError::InvalidPath);
+    }
+
+    let mut file = File::open(path)?;
+    extract_config(&mut file)
+}
+
+pub fn extract_config(file: &mut File) -> Result<Config, ConfigError> {
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents)?;
+
+    Ok(toml::from_str::<Config>(&file_contents)?)
+}
