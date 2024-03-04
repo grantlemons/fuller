4b9d15c3cf855bd576091bc507407bb206224839
Grant Lemons <grant0lemons@gmail.com>
2024-03-04 14:45:43 -0700

    add view for assignment

--- canvas_api/src/types/responses/assignment.rs
+++ canvas_api/src/types/responses/assignment.rs
@@ -1,5 +1,5 @@
 use super::Discussion;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Local, Utc};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -67,6 +67,36 @@ impl std::cmp::PartialEq for Assignment {
 impl std::fmt::Display for Assignment {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.name)
+    }
+}
+
+impl Assignment {
+    pub fn view(&self, config: &canvas_cli_config::Config) -> String {
+        let due_at = match self.due_at {
+            Some(date) => format!(
+                "\nDue At: {}",
+                DateTime::<Local>::from(date).format(&crate::datetime_format(config))
+            ),
+            None => String::default(),
+        };
+        let lock_explanation = match (self.lock_explanation.to_owned(), self.locked_for_user) {
+            (Some(explanation), true) => format!("\nAssignment is Locked!\n{}", explanation),
+            (_, true) => "\nAssignment is Locked!".to_owned(),
+            _ => String::default(),
+        };
+        let description = match self.description.to_owned() {
+            Some(description) => {
+                format!(
+                    "\n\n{}",
+                    html2text::from_read(&mut description.as_bytes(), 40)
+                )
+            }
+            None => String::default(),
+        };
+        format!(
+            "[{}] {}\nHTML Link: {}{}{}{}", // TODO: Investigate formatting w/ termcolor
+            self.id, self.name, self.html_url, due_at, lock_explanation, description
+        )
     }
 }
 
